abd193ce5c2a735262a75251812b163392f48cb9
Millenicide <590266+Skrell@users.noreply.github.com>
2024-10-22 10:58:38 -0400

    Add files via upload

---
+++ AgentRansackFilter.js
@@ -0,0 +1,12 @@
+﻿var regExp = new RegExp( SearchParms.ContainingTextCustomParm );
+
+function isValidLine( nLineNum, strText )
+{
+    var bIsValid = true;
+    try
+    {
+        bIsValid = !regExp.test( strText );
+    }
+    catch( e )        {}
+    return bIsValid;
+}
